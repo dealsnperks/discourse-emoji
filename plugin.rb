8246b6d946d8336ec172bda5591acb2aefbb492c
Régis Hanol <regis@hanol.fr>
2015-11-05 17:28:14 +0100

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: discourse-emoji
+# about: Boilerplate plugin to add custom emojis
+# version: 0.1
+# authors: Régis Hanol (zogstrip)
+# url: https://github.com/discourse/discourse-emoji
+
+enabled_site_setting :emoji_enabled
+
+register_emoji "discourse", "/plugins/discourse-emoji/images/discourse.png"
